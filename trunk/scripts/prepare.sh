a80aababa65561b72514a2186f3aa0d8d0029df5
black <black@0ce2ea08-208d-4007-b2ce-6ddd71483d27>
2009-11-22 09:06:58 +0000

    fixed bug in find statement;

--- trunk/scripts/prepare.sh
+++ trunk/scripts/prepare.sh
@@ -4,7 +4,7 @@ epydoc --check run-condensation.py lib condensation
 epydoc --config scripts/epydoc.config
 
 echo "cleaning up..."
-find . -iname "*~" -o -iname "*.pyc" -delete -print
+find . -iregex '.*\(~\|\.pyc\)$' -delete -print
 
 echo "Statistics:"
 echo -n "Lines: "
